81d1f7e61d6aea4a74715600a2cb25352b3b1971
nkk-personal <qa2dev4swift@gmail.com>
2021-12-28 17:35:40 -0800

    updated FeedItem and Loader

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Naveen Keerthy on 12/28/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
